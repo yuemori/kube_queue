70675671e94d56bc0812676fdfc4d4d86146d2ac
yuemori <yuemori@aiming-inc.com>
2019-08-12 03:11:51 +0900

    Introduce thor gem

--- docker/test_worker.rb
+++ docker/test_worker.rb
@@ -6,7 +6,7 @@ class TestWorker
   worker_name 'kube-queue-test'
   image ENV['IMAGE_NAME']
   container_name 'kube-queue-test'
-  command 'bundle', 'exec', 'kube_queue', 'TestWorker', '-r', './test_worker.rb'
+  command 'bundle', 'exec', 'kube_queue', 'runner', 'TestWorker', '-r', './test_worker.rb'
 
   def perform(payload)
     puts payload['message']
