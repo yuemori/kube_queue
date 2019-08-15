0971004ee54d34510485d32c66d590d5c7dfeafc
yuemori <yuemori@aiming-inc.com>
2019-08-15 17:07:31 +0900

    Fix build myapp image was failure

--- examples/myapp/config/initializers/kube_queue.rb
+++ examples/myapp/config/initializers/kube_queue.rb
@@ -1,5 +1,5 @@
 KubeQueue.kubernetes_configure do |client|
   client.url = ENV['K8S_URL']
   client.ssl_ca_file = ENV['K8S_CA_CERT_FILE']
-  client.auth_token = File.read(ENV['K8S_BEARER_TOKEN_FILE'])
+  client.auth_token = File.read(ENV['K8S_BEARER_TOKEN_FILE']) if File.exists?(ENV['K8S_BEARER_TOKEN_FILE'].to_s)
 end
